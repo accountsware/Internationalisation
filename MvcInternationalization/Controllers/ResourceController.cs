b6906816160c502d46c43c7ece39a09ee07cd641
accountsware <muhammad@accountsware.com>
2015-04-24 18:39:05 +0100

    Initial commit

---
+++ MvcInternationalization/Controllers/ResourceController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ExtensionMethods;
+
+using Resource = Resources.Resources;
+
+namespace MvcInternationalization.Controllers
+{
+    public class ResourceController : BaseController
+    {
+        // GET: /Resource/GetResources
+        const int durationInSeconds = 2 * 60 * 60;  // 2 hours.
+
+        [OutputCache(VaryByCustom = "culture", Duration = durationInSeconds)]
+        public JsonResult GetResources()
+        {
+
+            return Json(
+                typeof(Resource)
+                .GetProperties()
+                .Where(p => !p.Name.IsLikeAny("ResourceManager", "Culture")) // Skip the properties you don't need on the client side.
+                .ToDictionary(p => p.Name, p => p.GetValue(null) as string)
+                 , JsonRequestBehavior.AllowGet);
+
+            // Or the following 
+            /*
+            return Json(new Dictionary<string, string> { 
+                {"Age", Resource.Age},
+                {"FirstName", Resource.FirstName},
+                {"LastName", Resource.LastName},
+                {"EnterNumber", Resource.EnterNumber}
+                   
+            }, JsonRequestBehavior.AllowGet);
+            */
+
+
+        }
+    }
+}
---
+++ MvcInternationalization/Global.asax.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using MvcInternationalization.Helpers;
+
+namespace MvcInternationalization
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        public override string GetVaryByCustomString(HttpContext context, string custom)
+        {            
+            if (custom == "culture") // culture name (e.g. "en-US") is what should vary caching
+            {
+                string cultureName = null;
+              
+                // Attempt to read the culture cookie from Request
+                HttpCookie cultureCookie = Request.Cookies["_culture"];
+                if (cultureCookie != null) {
+                    cultureName = cultureCookie.Value;
+                }
+                else {
+                    cultureName = Request.UserLanguages != null && Request.UserLanguages.Length > 0 ? Request.UserLanguages[0] : null; // obtain it from HTTP header AcceptLanguages
+                }
+                
+                // Validate culture name
+                cultureName = CultureHelper.GetImplementedCulture(cultureName);
+
+                return cultureName.ToLower(); // use culture name as the cache key, "es", "en-us", "es-cl", etc.
+            }
+
+            return base.GetVaryByCustomString(context, custom);
+        } 
+    }
+}
